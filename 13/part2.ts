7e1705a4439081f69563d333f7f9e5bf90ed0d58
Mikk Kärner <k2rnermikk@gmail.com>
2023-12-15 16:02:02 +0200

    Day 13

---
+++ 13/part2.ts
@@ -0,0 +1,93 @@
+// const rawContent = Deno.readTextFileSync('input_1_test.txt')
+const rawContent = Deno.readTextFileSync('input_1.txt')
+
+// console.log(rawContent)
+// console.log('------------------')
+
+type MapSymbol = '#' | '.'
+type Map = MapSymbol[][]
+
+const maps = rawContent
+  .split('\n\n')
+  .map((line) => line.split('\n').map((line) => line.split('') as MapSymbol[]))
+
+function countDifference(string1: string, string2: string): number {
+  let differences = 0
+
+  for (let i = 0; i < string1.length; i++) {
+    if (string1[i] !== string2[i]) {
+      differences++
+    }
+  }
+
+  return differences
+}
+
+function findReflectionIndex(map: Map): number {
+  const rowsAsStrings = map.map((row) => row.join(''))
+
+  let mirrorStartIndex = -1
+
+  // front to back
+  for (let i = 1; i < rowsAsStrings.length; i++) {
+    const string1 = rowsAsStrings.slice(0, i).join('')
+    const string2 = rowsAsStrings
+      .slice(i, 2 * i)
+      .reverse()
+      .join('')
+
+    if (countDifference(string1, string2) === 1) {
+      mirrorStartIndex = i - 1
+      break
+    }
+  }
+
+  if (mirrorStartIndex !== -1) {
+    return mirrorStartIndex
+  }
+
+  // back to front
+  for (let i = 1; i < rowsAsStrings.length; i++) {
+    const string1 = rowsAsStrings
+      .slice(rowsAsStrings.length - i, rowsAsStrings.length)
+      .reverse()
+      .join('')
+    const string2 = rowsAsStrings
+      .slice(rowsAsStrings.length - 2 * i, rowsAsStrings.length - i)
+      .join('')
+
+    if (countDifference(string1, string2) === 1) {
+      mirrorStartIndex = rowsAsStrings.length - i - 1
+      break
+    }
+  }
+
+  return mirrorStartIndex
+}
+
+function flipAxes(map: Map): Map {
+  return map[0].map((_, index) => map.map((row) => row[index]).reverse())
+}
+
+const sum = maps
+  .map((map) => {
+    const reflectionIndex = findReflectionIndex(map)
+
+    if (reflectionIndex === -1) {
+      return {
+        reflectionNumber: findReflectionIndex(flipAxes(map)) + 1,
+        multiplier: 1,
+      }
+    }
+    return {
+      reflectionNumber: reflectionIndex + 1,
+      multiplier: 100,
+    }
+  })
+  .reduce(
+    (acc, { reflectionNumber, multiplier }) =>
+      acc + reflectionNumber * multiplier,
+    0
+  )
+
+console.log(sum)
